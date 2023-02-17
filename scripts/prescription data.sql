1528356605f80d866c9fad3a0fc9d7f574452b32
brandylyn <brandylynlanders@gmail.com>
2023-02-16 20:05:26 -0600

    first push Thursday

--- scripts/prescription data.sql
+++ scripts/prescription data.sql
@@ -69,12 +69,37 @@ LIMIT 25;
 
 -- 3. 
 --     a. Which drug (generic_name) had the highest total drug cost?
+SELECT SUM(total_drug_cost)AS total_cost, drug.generic_name
+FROM prescriber 
+INNER JOIN prescription 
+ON prescriber.npi=prescription.npi
+INNER JOIN drug
+ON prescription.drug_name=drug.drug_name
+GROUP BY drug.generic_name
+ORDER BY total_cost DESC
+LIMIT 25;
 
 --     b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
-
+SELECT *
+FROM prescriber 
+INNER JOIN prescription 
+ON prescriber.npi=prescription.npi
+INNER JOIN drug
+ON prescription.drug_name=drug.drug_name
+GROUP BY prescriber.specialty_description
+ORDER BY total_claims DESC
+LIMIT 25;
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
-
+SELECT *
+FROM prescriber 
+INNER JOIN prescription 
+ON prescriber.npi=prescription.npi
+INNER JOIN drug
+ON prescription.drug_name=drug.drug_name
+GROUP BY prescriber.specialty_description
+ORDER BY total_claims DESC
+LIMIT 25;
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
 -- 5. 
